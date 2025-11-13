6ac010365f37891f827b25f2e958ba1a6cb90691
JPLDN <149190497+JPLDN@users.noreply.github.com>
2025-11-13 17:59:53 +0000

    basic enemy movement working and done

--- Assets/Scripts/Enemies/BasicEnemyMovement.cs
+++ Assets/Scripts/Enemies/BasicEnemyMovement.cs
@@ -1,33 +1,26 @@
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class BasicEnemyMovement : MonoBehaviour
 {
-    public float moveSpeed = 2f;
+    public float speed = 2f;
     public Transform groundCheck;
-    public float groundCheckDistance = 0.4f;
     public LayerMask groundLayer;
-    public Transform frontCheck;
-    public float wallCheckDistance = 0.1f;
 
     private Rigidbody2D rb;
     private bool movingRight = true;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2((movingRight ? 1 : -1) * moveSpeed, 0f);
+        rb.linearVelocity = new Vector2((movingRight ? 1 : -1) * speed, rb.linearVelocity.y);
 
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayer);
-        RaycastHit2D frontInfo = Physics2D.Raycast(frontCheck.position, Vector2.right * (movingRight ? 1 : -1), wallCheckDistance, groundLayer);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundCheck.position, Vector2.down, 1f, groundLayer);
 
-        if (groundInfo.collider == null || frontInfo.collider != null)
+        if (groundInfo.collider == false)
         {
             Flip();
         }
@@ -35,7 +28,7 @@ public class BasicEnemyMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             Flip();
         }
@@ -44,6 +37,8 @@ public class BasicEnemyMovement : MonoBehaviour
     void Flip()
     {
         movingRight = !movingRight;
-        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
     }
 }
